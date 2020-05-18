6a59387854247f5dc73a53b7fef269b914f381b0
Kalkovets <Wetal.calkovets@gmail.com>
2020-05-18 23:37:08 +0300

    Finishing registation page design, starting creating page with collecting user data.

---
+++ app/src/main/java/com/example/mifittracker/CollectingUserData.java
@@ -0,0 +1,14 @@
+package com.example.mifittracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectingUserData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collecting_user_data);
+    }
+}
