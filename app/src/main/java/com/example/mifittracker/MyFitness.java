7d356142aaf30241e28a6eeee0d9f736b3982fc2
Kalkovets <Wetal.calkovets@gmail.com>
2020-05-20 15:43:10 +0300

    Starting work with Google Fit API

---
+++ app/src/main/java/com/example/mifittracker/MyFitness.java
@@ -0,0 +1,86 @@
+package com.example.mifittracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.BleDevice;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.BleScanCallback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Arrays;
+
+public class MyFitness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_fitness);
+
+        System.out.println("before something");
+        do_something();
+        System.out.println("after something");
+    }
+
+    private void do_something() {
+        BleScanCallback bleScanCallbacks = new BleScanCallback() {
+            @Override
+            public void onDeviceFound(BleDevice device) {
+                System.out.println("onDeviceFound");
+                // A device that provides the requested data types is available
+            }
+            @Override
+            public void onScanStopped() {
+                System.out.println("onScanStopped");
+                // The scan timed out or was interrupted
+            }
+        };
+
+        TextView hrBRM = (TextView)findViewById(R.id.hrBPM);
+ //       hrBRM.setText();
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+
+        GoogleSignInAccount account = GoogleSignIn.getAccountForExtension(this, fitnessOptions);
+
+        if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
+            System.out.println("TRY GET PERMISSIONS");
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    1, // e.g. 1
+                    account,
+                    fitnessOptions);
+        } else {
+            System.out.println("SUCCESS");
+            //accessGoogleFit();
+        }
+
+        _response = Fitness.getBleClient(this, account)
+                .startBleScan(Arrays.asList(DataType.TYPE_HEART_RATE_BPM),
+                        1000, bleScanCallbacks);
+
+        _response.addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> var1) {
+                System.out.println("FUCKING SHIT");
+                System.out.println("EXCEPTION: " + var1.getException());
+                System.out.println("Status " + var1.isSuccessful());
+            }
+        });
+    }
+
+    private Task<Void> _response;
+
+}
