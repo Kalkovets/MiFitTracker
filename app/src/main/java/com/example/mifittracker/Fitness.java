20befe409c6ef5a4651126c73135ca7dbdc566ac
Kalkovets <Wetal.calkovets@gmail.com>
2020-05-20 11:26:15 +0300

    Start parsing data from fitness tracker

---
+++ app/src/main/java/com/example/mifittracker/Fitness.java
@@ -0,0 +1,13 @@
+package com.example.mifittracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fitness extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness);
+    }
+}
